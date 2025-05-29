c08a746734985710f348963e50debd14b4226bfa
Hoang Yen Nhi <yennhi03013009@gmail.com>
2025-05-29 23:17:15 +0700

    edit config

--- src/main/java/org/example/condomanagement/util/Constants.java
+++ src/main/java/org/example/condomanagement/util/Constants.java
@@ -2,7 +2,7 @@ package org.example.condomanagement.util;
 
 public class Constants {
     public static final String APP_TITLE = "Quản lý thu phí chung cư";
-    public static final String DB_URL = "jdbc:postgresql://localhost:5432/condo_fees";
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/bluemoon";
     public static final String DB_USER = "postgres";
     public static final String DB_PASS = "272004";
 }
